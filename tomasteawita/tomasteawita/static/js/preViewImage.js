88b7d5f13b2214066985580fb77c50f8daeac71c
Cueva Tomas Santiago <tomassantiagocueva@gmail.com>
2022-11-21 08:01:31 -0300

    Se dio estilos a uploadPost

--- tomasteawita/tomasteawita/static/js/preViewImage.js
+++ tomasteawita/tomasteawita/static/js/preViewImage.js
@@ -1,7 +1,21 @@
 const $id_image = document.querySelector("#id_image"),
   $imagePreView = document.querySelector("#imagePreView");
 
-  
+document.getElementById('id_description').remove()
+
+var formBody = document.getElementById("formBody")
+
+var texatArea = document.createElement("textarea")
+texatArea.setAttribute('id','id_description')
+texatArea.setAttribute('value','Escribe la descripcion del post')
+texatArea.setAttribute('name','description')
+texatArea.setAttribute('type','text')
+texatArea.setAttribute('maxlength','1024')
+
+formBody.children[1].appendChild(texatArea)
+
+
+
 $id_image.addEventListener("change", () => {
   const field = $id_image.files;
   if (!field || !field.length) {
